70a9f015e4cbf553439e69e03a13fe7ab91e207c
Tigran Meliksetyants <tigran338@gmail.com>
2023-03-23 14:11:36 -0700

    User guide

--- My project/Assets/Scripts/CheckersBasic.cs
+++ My project/Assets/Scripts/CheckersBasic.cs
@@ -6,14 +6,53 @@ using UnityEngine;
 
 public class CheckersBasic : MonoBehaviour
 {
-    public GameObject whiteQueen, blackQueen, whitePiece, blackPiece;
-    GameObject[,] pieces = new GameObject[8, 8];
+    public GameObject whiteQueen, blackQueen, whitePiece, blackPiece, visualizePiece;
+    
+    private GameObject[,] pieces = new GameObject[8, 8];
+
+    private GameObject visualization;
     private Vector3 boardCornerleftbottom = new Vector3(-0.210f, 0.1f, 0.16f);
     private Vector3 boardCornerrightupper = new Vector3(-0.216f, 0.1f, -0.259f);
     private Vector3[,] postionCoordinates = new Vector3[8, 8];
 
 
     void Start()
+    {
+        boardInitialize();
+
+        //Initialize visualization GameObject
+        //______________________________________________________________
+        visualization = GameObject.Instantiate(visualizePiece);
+        visualization.transform.SetParent(transform);
+        visualization.layer = LayerMask.NameToLayer("Ignore Raycast");
+        //______________________________________________________________
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log(coursorPosition());
+        }
+        userGuide();
+
+    }
+    
+
+
+    private void generatePiece(GameObject pieceType, int x, int y)
+    {
+        GameObject go = GameObject.Instantiate(pieceType);
+        go.transform.SetParent(transform);
+        go.transform.localPosition = postionCoordinates[x, y];
+        go.layer = LayerMask.NameToLayer("Ignore Raycast");
+        pieces[x,y] = go;
+    }
+
+    //Puting pieces on there places to start the game
+    private void boardInitialize()
     {
         //Fill up postionCoordinates array
         for (int i = 0; i < 8; i++)
@@ -31,7 +70,7 @@ public class CheckersBasic : MonoBehaviour
             for (int j = 0; j < 8; j++)
             {
                 if ((i + j) % 2 == 1)
-                    GeneratePiece(whitePiece, i, j);
+                    generatePiece(whitePiece, i, j);
             }
         }
 
@@ -42,20 +81,13 @@ public class CheckersBasic : MonoBehaviour
             for (int j = 0; j < 8; j++)
             {
                 if ((i + j) % 2 == 1)
-                    GeneratePiece(blackPiece, i, j);
+                    generatePiece(blackPiece, i, j);
             }
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonUp(0))
-        {
-            Debug.Log(clickedPosition());
-        }
-    }
-    private Vector2 clickedPosition()
+    
+    //Return the board cell where cursor is pointing
+    private Vector2 coursorPosition()
     {
         Vector2 closestPosition = new Vector2(-1, -1);
         RaycastHit hitInfo = new RaycastHit();
@@ -64,11 +96,11 @@ public class CheckersBasic : MonoBehaviour
         if (hitInfo.transform.tag.Equals("Base"))
         {
             float mindist = float.MaxValue;
-            Debug.Log(transform.InverseTransformPoint(hitInfo.point));
+            //Debug.Log(transform.InverseTransformPoint(hitInfo.point));
             for (int i = 0; i < 8; i++)
             {
-               
-                
+
+
                 for (int j = 0; j < 8; j++)
                 {
                     if (Math.Abs(Vector3.Distance(transform.InverseTransformPoint(hitInfo.point), postionCoordinates[i, j] + new Vector3(0, 0, (Math.Abs(boardCornerleftbottom.x) + Math.Abs(boardCornerrightupper.x)) / 7))) < mindist)
@@ -83,14 +115,22 @@ public class CheckersBasic : MonoBehaviour
         return closestPosition;
     }
 
-
-    private void GeneratePiece(GameObject pieceType, int x, int y)
+    private void userGuide()
     {
-        GameObject go = GameObject.Instantiate(pieceType);
-        go.transform.SetParent(transform);
-        go.transform.localPosition = postionCoordinates[x, y];
-        go.layer = LayerMask.NameToLayer("Ignore Raycast");
-        pieces[x,y] = go;
+        Vector2 position = coursorPosition();
+        if (position == new Vector2(-1, -1) || pieces[(int)position.x, (int)position.y] == null)
+        {
+            visualization.transform.position = Vector3.zero;
+            return;
+        }
+
+        visualization.transform.localPosition = postionCoordinates[(int)position.x, (int)position.y];
+        Debug.Log(convertToVector3(position));
+    }
+
+    private Vector3 convertToVector3(Vector2 v)
+    {
+        return new Vector3(v.x, boardCornerleftbottom.y, v.y);
     }
 }
 
