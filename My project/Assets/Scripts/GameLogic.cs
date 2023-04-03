049a80328e2b584e65c4b46e7f65eef18a2ad0ae
CyberBinary <42885189+CyberBinary@users.noreply.github.com>
2023-04-03 14:59:29 -0700

    CheckCapturableBoard() added after moves

--- My project/Assets/Scripts/GameLogic.cs
+++ My project/Assets/Scripts/GameLogic.cs
@@ -136,6 +136,7 @@ public class GameLogic
         }
 
         turnWhite = !turnWhite;
+        CheckCapturableBoard();
         return;
     }
 
@@ -273,6 +274,7 @@ public class GameLogic
         {
             turnWhite = !turnWhite;
             mustCapture = false;
+            CheckCapturableBoard();
         }
     }
 
