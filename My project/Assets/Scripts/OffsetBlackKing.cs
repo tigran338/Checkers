b382c33662a4f5b04c6c747d6de716f45a7f88a9
Tigran Meliksetyants <tigran338@gmail.com>
2023-05-04 15:14:53 -0700

    King size update

--- My project/Assets/Scripts/OffsetBlackKing.cs
+++ My project/Assets/Scripts/OffsetBlackKing.cs
@@ -7,6 +7,7 @@ public class OffsetBlackKing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position += new Vector3(-0.007142f, 0, 0.0732857f);
+        //this.transform.position += new Vector3(-0.007142f, 0, 0.0732857f);
+        this.transform.position += new Vector3(-0.0056428f, 0, 0.0793857f);
     }
 }
--- My project/Assets/Scripts/OffsetWhiteKing.cs
+++ My project/Assets/Scripts/OffsetWhiteKing.cs
@@ -7,7 +7,9 @@ public class OffsetWhiteKing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position += new Vector3(-0.00548596f, 0, 0.0266f);
+        //this.transform.position += new Vector3(-0.00548596f, 0, 0.0266f);
+        //this.transform.position += new Vector3(-0.00358596f, 0, 0.0133572f);
+        this.transform.position += new Vector3(-0.0032568f, 0, 0.0208f);
     }
 
     
